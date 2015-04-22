a9584ea16d6ca7700517643d9b66ee462c42ce37
Kelly Innes <kinnes@gmail.com>
2015-04-21 23:36:42 -0400

    really BASIC admin authentication

--- main.rb
+++ main.rb
@@ -1,11 +1,16 @@
 require 'sinatra'
 
-get '/' do
-  erb :index, :locals => {'current' => '/'}
+configure do
+  enable :sessions
+  # don't use these in production obviously
+  set :username, 'admin'
+  set :password, 'admin'
 end
 
-get '/signin' do
-  erb :signin, :locals => {'current' => '/signin'}
+# basic routes
+
+get '/' do
+  erb :index, :locals => {'current' => '/'}
 end
 
 get '/about' do
@@ -16,10 +21,37 @@ get '/contact' do
   erb :contact, :locals => {'current' => '/contact'}
 end
 
-get '/test' do
-  erb :test, :locals => {'current' => '/test'}
+get '/four_oh_one' do
+  erb :four_oh_one, :locals => {'current' => '/four_oh_one'}
 end
 
 not_found do
   erb :four_oh_four, :locals => {'current' => '/four_oh_four'}
+end
+
+# protected route
+
+get '/test' do
+  redirect to('/four_oh_one') unless session[:admin]
+  erb :test, :locals => {'current' => '/test'}
+end
+
+# authentication routes
+
+get '/signin' do
+  erb :signin, :locals => {'current' => '/signin'}
+end
+
+post '/signin' do
+  if params[:username] == settings.username && params[:password] == settings.password
+    session[:admin] = true
+    redirect to('/test')
+  else
+    erb :signin
+  end
+end
+
+get '/signout' do
+  session.clear
+  redirect to('/')
 end
