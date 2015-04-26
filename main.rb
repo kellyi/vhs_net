886c45890bec25d64a30c494784db9dd661aac3d
Kelly Innes <kinnes@gmail.com>
2015-04-26 10:39:27 -0400

    routes and form for editing records

--- main.rb
+++ main.rb
@@ -30,6 +30,8 @@ get '/test' do
   erb :test, :locals => {'current' => '/test'}
 end
 
+# list routes
+
 get '/list' do
   redirect to('/four_oh_one') unless session[:admin]
   @list = List.all
@@ -44,17 +46,52 @@ end
 post '/list/add' do
   redirect to('/four_oh_one') unless session[:admin]
   item = List.new
-  item.item = params[:name]
+  if params[:name].length >= 50
+    item.item = params[:name][0..48]
+  else
+    item.item = params[:name]
+  end
   item.added_on = Time.now
-  item.note = params[:notes]
+  if params[:note].length >= 50
+    item.note = params[:note][0..48]
+  else
+    item.note = params[:note]
+  end
   item.quantity = params[:quantity]
   item.save
+  redirect to('/list')
+end
+
+get '/list/:id' do
+  redirect to('four_oh_one') unless session[:admin]
   redirect to('/list')
 end
 
 get '/:id/list/destroy' do
   redirect to('/four_oh_one') unless session[:admin]
   List.get(params[:id]).destroy
+  redirect to('/list')
+end
+
+get '/edit/:id' do
+  redirect to('/four_oh_one') unless session[:admin]
+  @item = List.get(params[:id])
+  erb :edit_item, :locals => {'current' => '/list'}
+end
+
+post '/edit/:id' do
+  item = List.get(params[:id])
+  if params[:name].length >= 50
+    item.item = params[:name][0..48]
+  else
+    item.item = params[:name]
+  end
+  if params[:note].length >= 50
+    item.note = params[:note][0..48]
+  else
+    item.note = params[:note]
+  end
+  item.save
   redirect to('/list')
 end
 
